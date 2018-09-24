2ca460b86843e641194bb23151ae4a7424efa711
Blazer <sstubbs012@gmail.com>
2018-09-24 19:50:49 -0400

    new modal for no results

--- Assets/javascript/javascript.js
+++ Assets/javascript/javascript.js
@@ -40,6 +40,7 @@ $(document).ready(function() {
                 url: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + city + "&limit=10&categories=" + foodCat + "&sort_by=rating",
                 success: function(data, status) {
                     console.log(data.businesses.length);
+                    var compLength = data.businesses.length;
                     for (var i = 0; i < data.businesses.length; i++) {
                         //company name, rating, address
                         var compName = data.businesses[i].name;
@@ -57,11 +58,31 @@ $(document).ready(function() {
                         // concatenating the results into one line
                         $("#api-results").append(`<p> ${compName} | Rating: ${compRating} | Review Count: ${reviewCount} | ${compAddress} </p>`); 
 
-
-                        // console.log("The returned data", data);
-                    //company name, rating, address
                     };
                     console.log("The returned data", data);
+                    if (compLength === 0) {
+                        var modal = document.getElementById('theModal');
+
+                        // Get the <span> element that closes the modal
+                        var span = document.getElementsByClassName("close")[0];
+
+                        // When the user clicks the button, open the modal 
+                            modal.style.display = "block";
+                            
+                        
+
+                        // When the user clicks on <span> (x), close the modal
+                        span.onclick = function() {
+                            modal.style.display = "none";
+                        }
+
+                        // When the user clicks anywhere outside of the modal, close it
+                        window.onclick = function(event) {
+                            if (event.target == modal) {
+                                modal.style.display = "none";
+                            }
+                        }
+                    }
 
                 },
                 beforeSend: function(xhr, settings) { xhr.setRequestHeader('Authorization', 'Bearer ' + apiKEY); }
